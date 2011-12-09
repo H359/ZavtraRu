1bc3d6b07613b4e4b37d3c24f6690ca1bb4e0d89
zw0rk <ostronom@gmail.com>
2011-12-09 11:01:16 +0400

    quote timeout fixed

--- zavtra/corecontent/context_processors.py
+++ zavtra/corecontent/context_processors.py
@@ -13,7 +13,7 @@ def common_pieces(request):
     top_rubrics  = cached(lambda: list(Rubric.objects.filter(on_top=True).exclude(title=u'Новости')), 'top_rubrics')
     featured = cached(lambda: list(FeaturedItems.objects.filter(is_active=True)), 'featured')
     try:
-	quote = cached(lambda: list(DailyQuote.objects.filter(day=datetime.now().date())), 'quote')[0]
+	quote = cached(lambda: list(DailyQuote.objects.filter(day=datetime.now().date())), 'quote', duration=600)[0]
     except IndexError:
 	quote = None
     return {
