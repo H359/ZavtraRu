23ca9c8f0d6e4e724121da627e0c6b56abd02ec1
zw0rk <ostronom@gmail.com>
2011-12-11 22:51:24 +0400

    Last hope :)

--- zavtra/forms.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from registration.forms import RegistrationFormNoFreeEmail
-from captcha.fields import ReCaptchaField
-
-class ReCaptchaForm(RegistrationFormNoFreeEmail):
-    bad_domains = ['email.com', '20minutemail.com']
-    captcha = ReCaptchaField(label=u'Проверим вашу человечность', attrs={'theme':'clean'}, help_text=u'Введите слова с картинки')
