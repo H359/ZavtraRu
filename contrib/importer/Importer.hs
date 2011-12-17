e475c6e81a94f7add01e16008aa03eba2b1b8149
zw0rk <ostronom@gmail.com>
2011-12-18 01:29:08 +0400

    :-D

--- contrib/importer/Importer.hs
+++ contrib/importer/Importer.hs
@@ -5,13 +5,15 @@ import System.Directory
 import qualified Data.ByteString.Lazy as B
 import Control.Applicative
 
-analyze :: FilePath -> IO ()
-analyze filepath = do
-    putStr $ filepath ++ "\n"
+analyze :: Archive -> FilePath -> IO ()
+analyze archive filepath = do
+    case findEntryByPath filepath archive of
+        Nothing -> putStr "oops\n"
+        Just f  -> putStr $ (eRelativePath f) ++ " " ++ (show $ eCompressedSize f) ++ " " ++ (show $ eUncompressedSize f) ++ "\n"
 
 main :: IO ()
 main = do
     archive <- toArchive <$> B.readFile "/home/zw0rk/Work/zavtra_archive/data/zavtra/00/318.zip"
     let files = filesInArchive archive
-    mapM_ analyze files
+    mapM_ (analyze archive) files
     putStr "done :-D"
