863c358aa80481b5d7d4e231603a4e6647b8221e
zw0rk <ostronom@gmail.com>
2012-01-07 16:22:11 +0400

    search pagination query typo

--- zavtra/ajax_filtered_fields/forms/__init__.py
+++
@@ -1,7 +0,0 @@
-from widgets import FilteredSelectMultiple, FilteredSelect
-from fields import (
-    AjaxManyToManyField, AjaxForeignKeyField, 
-    ManyToManyByLetter, ForeignKeyByLetter,
-    ManyToManyByStatus, ForeignKeyByStatus,
-    ManyToManyByRelatedField, ForeignKeyByRelatedField
-    )
