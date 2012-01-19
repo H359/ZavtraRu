87ba36deac73eb33c6ba6662c4e2e9778f0067f2
zw0rk <ostronom@gmail.com>
2012-01-19 16:58:45 +0400

    HOWDY?!

--- zavtra/corecontent/context_processors.py
+++ zavtra/corecontent/context_processors.py
@@ -22,8 +22,8 @@ def common_pieces(request):
     top_rubrics  = cached(lambda: list(Rubric.objects.filter(on_top=True).exclude(title=u'Новости')), 'top_rubrics', duration=600)
     featured = cached(lambda: list(FeaturedItems.objects.filter(is_active=True)), 'featured', duration=60)
     try:
-	quote = cached(lambda: DailyQuote.objects.filter(date = now.date()).latest('day'), 'quote', duration=600)
-    except DailyQuote.DoesNotExist:
+	quote = cached(lambda: DailyQuote.objects.filter(day=now.date()), 'quote', duration=600)[0]
+    except IndexError:
 	quote = None
     news = cached(lambda: map(news_stripper, ContentItem.objects.filter(enabled=True, pub_date__lte=now, rubric__title=u'Новости')[0:6]), 'news', duration=120)
     return {
