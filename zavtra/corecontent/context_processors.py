8ea10e5b7284f04e43847596fb98760b0b9f1d90
zw0rk <ostronom@gmail.com>
2012-01-19 16:37:52 +0400

    moar

--- zavtra/corecontent/context_processors.py
+++ zavtra/corecontent/context_processors.py
@@ -22,7 +22,7 @@ def common_pieces(request):
     top_rubrics  = cached(lambda: list(Rubric.objects.filter(on_top=True).exclude(title=u'Новости')), 'top_rubrics', duration=600)
     featured = cached(lambda: list(FeaturedItems.objects.filter(is_active=True)), 'featured', duration=60)
     try:
-	quote = cached(lambda: DailyQuote.objects.filter(date__lte = now.date()).latest('day'), 'quote', duration=600)
+	quote = cached(lambda: DailyQuote.objects.filter(date = now.date()).latest('day'), 'quote', duration=600)
     except DailyQuote.DoesNotExist:
 	quote = None
     news = cached(lambda: map(news_stripper, ContentItem.objects.filter(enabled=True, pub_date__lte=now, rubric__title=u'Новости')[0:6]), 'news', duration=120)
