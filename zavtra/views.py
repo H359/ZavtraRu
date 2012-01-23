da891c75835ebc539c5d77cd1433da05992b1724
zw0rk <ostronom@gmail.com>
2012-01-23 17:30:06 +0400

    !

--- zavtra/views.py
+++ zavtra/views.py
@@ -140,11 +140,11 @@ def live(request):
 def live_update(request):
     qty = int(request.GET.get('qty', 20))
     start = request.GET.get('start')
-    comments = Comment.objects.filter(enabled=True).order_by('-id')
+    comments = Comment.objects.filter(enabled=True).order_by('id')
     if start is None or start == 'null':
 	comments = comments.filter(created_at__gte = datetime.now() - timedelta(days=1))
     else:
-	comments = comments.filter(created_ad__gte = datetime.fromtimestamp(int(start)))
+	comments = comments.filter(created_at__gte = datetime.fromtimestamp(int(start)))
     return {
 	'stream': map(lambda c: render_to_string('comments/item.html', {'stream': True, 'comment':c}), comments[0:qty]),
     }
