6c3ad97c5af3abd34cbb925876e50a9fee92e5f4
zw0rk <ostronom@gmail.com>
2012-01-24 15:27:59 +0400

    13-profiling

--- zavtra/settings.py
+++ zavtra/settings.py
@@ -189,7 +189,7 @@ DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.sql.SQLDebugPanel',
     'debug_toolbar.panels.signals.SignalDebugPanel',
     'debug_toolbar.panels.logger.LoggingPanel',
-    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+    'profiling.ProfilingDebugPanel',
 )
 
 #PIPELINE = True
