b9a06b3df37f51bfb1da561dcef38f9fb918df60
zw0rk <ostronom@gmail.com>
2012-01-24 15:24:37 +0400

    profiling panel

--- zavtra/settings.py
+++ zavtra/settings.py
@@ -179,6 +179,19 @@ LOGGING = {
 CONVERT_FILENAME = True
 FILEBROWSER_EXCLUDE = []
 
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+)
+
 #PIPELINE = True
 PIPELINE_AUTO = False
 PIPELINE_VERSION = True
