cf7e2dfdbe77d9af10227e0b580f7d4360e7df29
zw0rk <ostronom@gmail.com>
2012-01-25 19:46:28 +0400

    html emails

--- zavtra/mail/management/commands/send_emails.py
+++ zavtra/mail/management/commands/send_emails.py
@@ -3,7 +3,8 @@
 #from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
 #from django.db import transaction
 from mail.models import EmailQueue
 
@@ -12,6 +13,7 @@ class Command(BaseCommand):
 	# SET SEMAPHORE!!!
 	q = EmailQueue.objects.filter(sent_at=None)[0:50]
 	for m in q:
-	    msg = EmailMessage(m.subject, m.body, m.from_field, [m.to_field], headers={'Content-Type': 'text/html'})
+	    msg = EmailMultiAlternatives(m.subject, strip_tags(m.body), m.from_field, [m.to_field])
+	    msg.attach_alternative(m.body, 'text/html')
 	    msg.send()
 	    m.mark_sent()
