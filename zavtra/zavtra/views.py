fd4d1dc81018a06de07a199591d386f52e37c2ea
zw0rk <ostronom@gmail.com>
2012-05-17 15:47:01 +0400

    users fix

--- zavtra/zavtra/views.py
+++ zavtra/zavtra/views.py
@@ -7,6 +7,6 @@ from articles.models import Article, Rubric
 def home(request):
     return {
         'rubrics': Rubric.objects.all()[0:6],
-        'columnists': User.objects.filter(firstname__isnull=False, lastname__isnull=False)[0:5],
+        'columnists': User.objects.filter(first_name__isnull=False, last_name__isnull=False)[0:5],
         'articles': Article.archive.all()[0:10]
     }
