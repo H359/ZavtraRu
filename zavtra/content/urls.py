3855d82f4421231eab9fa111183d35e9eb4b2487
ostronom <ostronom@gmail.com>
2013-02-15 00:54:47 +0400

    archive

--- zavtra/content/urls.py
+++ zavtra/content/urls.py
@@ -7,7 +7,7 @@ from content.views import ArticleView, RubricView, IssueView,\
 urlpatterns = patterns('',
   url(r'^issue/$', 'content.views.current_issue_redirect', name='content.views.current_issue'),
   url(r'^issue/(?P<year>[0-9]+)/(?P<issue>[0-9]+)/$', IssueView.as_view(), name='content.views.zeitung'),
-  url(r'^archive/((?P<id>[0-9]+)/){0,1}$', ArchiveView.as_view(), name='content.views.archive'),
+  url(r'^archive/$', ArchiveView.as_view(), name='content.views.archive'),
   url(r'^view/(?P<slug>[^/]+)/$', ArticleView.as_view(), name='content.views.article'),
   url(r'^rubric/(?P<slug>[^/]+)/$', RubricView.as_view(), name='content.views.rubric'),
   url(r'^events/$', EventsView.as_view(), name='content.views.events'),
