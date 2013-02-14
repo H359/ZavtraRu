5bc73f1633f48869d022cab17f218bc6ef13da64
ostronom <ostronom@gmail.com>
2013-02-14 23:45:25 +0400

    dates in community

--- zavtra/siteuser/views.py
+++ zavtra/siteuser/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
 from zavtra.paginator import QuerySetDiggPaginator as DiggPaginator
+from zavtra.utils import oneday
 
 from siteuser.models import User
 from content.models import Article
@@ -59,14 +60,29 @@ class CommunityView(ListView):
   template_name = 'siteuser/community.jhtml'
   paginate_by = 15
   paginator_class = DiggPaginator
+  selected_date = None
 
   def get_queryset(self):
-    return Article.published.\
-           filter(authors__level__gte = User.USER_LEVELS.trusted).\
-           prefetch_related('authors').defer('content')
+    qs = Article.published.\
+         filter(authors__level__gte = User.USER_LEVELS.trusted).\
+         prefetch_related('authors').defer('content')
+    if 'year' in self.request.GET and \
+       'month' in self.request.GET and \
+       'day' in self.request.GET:
+       dt = datetime(
+        year = int(self.request.GET['year']),
+        month = int(self.request.GET['month']),
+        day = int(self.request.GET['day']),
+        hour = 0, minute = 0, second = 0
+       )
+       qs = qs.filter(published_at__gte = dt, published_at__lt = dt + oneday)
+       self.selected_date = dt
+    return qs
 
   def get_context_data(self, **kwargs):
     context = super(CommunityView, self).get_context_data(**kwargs)
     context['alphabet'] = RU_ALPHABET
+    if self.selected_date is not None:
+      context['selected_date'] = self.selected_date
     context['most_commented'] = Article.get_most_commented()
     return context
