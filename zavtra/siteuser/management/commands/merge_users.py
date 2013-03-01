001ef6392530842e814a9c6c5db15924268ea990
zw0rk <ostronom@gmail.com>
2013-03-01 16:48:41 +0400

    User merge script

---
+++ zavtra/siteuser/management/commands/merge_users.py
@@ -0,0 +1,14 @@
+from content.models import Article
+from siteuser.models import User
+
+
+class Command(BaseCommand):
+  def handle(self, *args, **kwargs):
+    user_from_id, user_to_id = args
+    user_from = User.objects.get(id=user_from_id)
+    user_to = User.objects.get(id=user_to_id) 
+    for a in Article.objects.filter(authors__in = [user_from]):
+      a.authors.remove(user_from)
+      a.authors.add(user_to)
+    user_from.delete()
+
