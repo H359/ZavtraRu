804997d826e96060d1655bf14c08065aad985a80
ostronom <ostronom@gmail.com>
2013-03-01 13:51:52 +0400

    OK

--- zavtra/siteuser/admin.py
+++ zavtra/siteuser/admin.py
@@ -6,10 +6,13 @@ from siteuser.models import User
 
 class UserAdmin(admin.ModelAdmin):
   search_fields = ('last_name', 'email')
+  exclude = ('password',)
 
+  """
   def save_model(self, request, obj, form, change):
     super(UserAdmin, self).save_model(request, obj, form, change)
     obj.set_password(form.cleaned_data['password'])
     obj.save()
+  """
 
 admin.site.register(User, UserAdmin)
