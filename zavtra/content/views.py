38c1ce8a2689dde9eeb41ebdd65f2a195370603e
ostronom <ostronom@gmail.com>
2013-11-18 18:41:49 +0400

    q list

--- zavtra/content/views.py
+++ zavtra/content/views.py
@@ -275,7 +275,7 @@ class SearchView(ListView):
   stopwords = u'а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with'.split(',')
 
   def prepare_query(self, q):
-    return [x for x in q.split(' ') if x not in self.stopwords]
+    return ' '.join([x for x in q.split(' ') if x not in self.stopwords])
 
   def get_queryset(self):
     self.category = self.kwargs.get('category')
@@ -283,7 +283,7 @@ class SearchView(ListView):
     #qs = SearchQuerySet().filter(content = AutoQuery(self.q)).\
     #     models(Article).\
     #     order_by('-pub_date')
-    qs = Article.published.filter(title__icontains=' '.join(self.q))
+    qs = Article.published.filter(title__icontains=self.q)
     if self.category is not None:
       qs = qs.filter(rubric__slug=self.category)
     return qs
